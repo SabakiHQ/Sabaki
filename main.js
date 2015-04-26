1cad339534b82f12b3f7785af91c47883311ace6
yishn <shen.yichuan@gmail.com>
2015-04-26 14:28:16 +0200

    No semicolon

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-var app = require('app');
-var BrowserWindow = require('browser-window');
+var app = require('app')
+var BrowserWindow = require('browser-window')
 
-var mainWindow = null;
+var mainWindow = null
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
     if (process.platform != 'darwin')
-        app.quit();
-});
+        app.quit()
+})
 
 app.on('ready', function() {
-    mainWindow = new BrowserWindow({ width: 800, height: 600 });
-    mainWindow.loadUrl('file://' + __dirname + '/index.html');
+    mainWindow = new BrowserWindow({ width: 800, height: 600 })
+    mainWindow.loadUrl('file://' + __dirname + '/index.html')
 
     mainWindow.on('closed', function() {
-        mainWindow = null;
-    });
-});
+        mainWindow = null
+    })
+})
