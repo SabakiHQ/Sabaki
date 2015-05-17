aebf7f8bd4853e1b47030763dac5f5531d113bc6
yishn <shen.yichuan@gmail.com>
2015-05-17 15:13:50 +0200

    Update setting module

--- module/setting.js
+++ module/setting.js
@@ -4,7 +4,15 @@ var path = require('path')
 var app = remote.require('app')
 
 var filename = path.join(app.getPath('userData'), 'settings.json')
-var settings = JSON.decode(fs.readFileSync(filename, { encoding: 'utf8' }))
+var settings = {}
+
+exports.load = function() {
+    settings = JSON.decode(fs.readFileSync(filename, { encoding: 'utf8' }))
+}
+
+exports.save = function() {
+    fs.writeFileSync(filename, JSON.encode(settings))
+}
 
 exports.get = function(key) {
     if (key in settings) return settings[key]
@@ -15,3 +23,10 @@ exports.set = function(key, value) {
     settings[key] = value
     fs.writeFileSync(filename, JSON.encode(settings))
 }
+
+exports.default = function(key, value) {
+    if (exports.get(key) == null) exports.set(key, value)
+}
+
+try { fs.accessSync(filename, fs.F_OK) } catch(err) { exports.save() }
+exports.load()
