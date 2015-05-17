fca7aee688400557a06266a7ed25373088a52dae
yishn <shen.yichuan@gmail.com>
2015-05-17 14:39:31 +0200

    Add setting module

---
+++ module/setting.js
@@ -0,0 +1,16 @@
+var remote = require('remote')
+var fs = require('fs')
+var path = require('path')
+var app = remote.require('app')
+
+var filename = path.join(app.getPath('userData'), 'settings.json')
+var settings = JSON.decode(fs.readFileSync(filename, { encoding: 'utf8' }))
+
+exports.get = function(key) {
+    return settings[key]
+}
+
+exports.set = function(key, value) {
+    settings[key] = value
+    fs.writeFileSync(filename, JSON.encode(settings))
+}
