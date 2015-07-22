99345a9f49ef69061b9882b729539ed6eff1cda4
yishn <shen.yichuan@gmail.com>
2015-07-22 19:45:01 +0200

    Manage functions

--- view/index.js
+++ view/index.js
@@ -14,6 +14,189 @@ var Board = require('../module/board.js')
 var Scrollbar = require('../lib/gemini-scrollbar')
 var Menu = remote.require('menu')
 
+/**
+ * Getter & setter
+ */
+
+function getRootTree() {
+    if (!getCurrentTreePosition()) return null
+
+    return getCurrentTreePosition().unpack(function(tree, index) {
+        while (tree.parent != null) tree = tree.parent
+        return tree
+    })
+}
+
+function setRootTree(tree) {
+    if (tree.nodes.length == 0) return
+
+    tree.parent = null
+    setCurrentTreePosition(sgf.addBoard(tree), 0)
+
+    // Update UI
+    if (getShowSidebar()) {
+        updateGraph()
+        updateSlider()
+    }
+
+    if ('PB' in tree.nodes[0]) setPlayerName(1, tree.nodes[0].PB[0])
+    if ('PW' in tree.nodes[0]) setPlayerName(-1, tree.nodes[0].PW[0])
+}
+
+function getGraphMatrixDict() {
+    return $('graph').retrieve('graphmatrixdict')
+}
+
+function setGraphMatrixDict(matrixdict) {
+    if (!getShowSidebar()) return
+
+    var s = $('graph').retrieve('sigma')
+    $('graph').store('graphmatrixdict', matrixdict)
+
+    s.graph.clear()
+    s.graph.read(gametree.matrix2graph(matrixdict))
+
+    s.refresh()
+}
+
+function setCurrentTreePosition(tree, index) {
+    if (!tree || getScoringMode()) return
+
+    // Remove current graph node color
+    var n = getCurrentGraphNode()
+    if (n && n != getGraphNode(tree, index)) delete n.color
+
+    $('goban').store('position', new Tuple(tree, index))
+
+    // Set current path
+    var t = tree
+    while (t.parent) {
+        t.parent.current = t.parent.subtrees.indexOf(t)
+        t = t.parent
+    }
+
+    // Update graph and slider
+    var n = getCurrentGraphNode()
+    if (n) {
+        setTimeout(function() {
+            if (getCurrentGraphNode() != n) return
+            centerGraphCameraAt(n)
+            updateSlider()
+        }, 300)
+    }
+
+    setBoard(sgf.addBoard(tree, index).nodes[index].board)
+
+    // Determine current player
+    setCurrentPlayer(1)
+
+    if ('B' in tree.nodes[index]) setCurrentPlayer(-1)
+    else if ('W' in tree.nodes[index]) setCurrentPlayer(1)
+    else if ('PL' in tree.nodes[index])
+        setCurrentPlayer(tree.nodes[index].PL[0] == 'W' ? -1 : 1)
+    else if ('HA' in tree.nodes[index] && tree.nodes[index].HA[0].toInt() >= 1)
+        setCurrentPlayer(-1)
+}
+
+function getCurrentTreePosition() {
+    return $('goban').retrieve('position')
+}
+
+function getCurrentGraphNode() {
+    if (!getCurrentTreePosition()) return null
+    return getCurrentTreePosition().unpack(getGraphNode)
+}
+
+function getGraphNode(tree, index) {
+    var s = $('graph').retrieve('sigma')
+    return s.graph.nodes(tree.id + '-' + index)
+}
+
+function getSelectedTool() {
+    var li = $$('#edit .selected')[0]
+    var tool = li.get('class').replace('selected', '').replace('-tool', '').trim()
+
+    if (tool == 'stone') {
+        return li.getElement('img').get('src').contains('_1') ? 'stone_1' : 'stone_-1'
+    } else {
+        return tool
+    }
+}
+
+function getBoard() {
+    return $('goban').retrieve('board')
+}
+
+function setBoard(board) {
+    if (!getBoard() || getBoard().size != board.size) {
+        $('goban').store('board', board)
+        buildBoard()
+    }
+
+    $('goban').store('board', board)
+    setCaptures(board.captures)
+
+    for (var x = 0; x < board.size; x++) {
+        for (var y = 0; y < board.size; y++) {
+            var li = $('goban').getElement('.pos_' + x + '-' + y)
+            var sign = board.arrangement[li.retrieve('tuple')]
+            var types = ['ghost_1', 'ghost_-1', 'circle', 'triangle',
+                'cross', 'square', 'label', 'point']
+
+            types.each(function(x) {
+                if (li.hasClass(x)) li.removeClass(x)
+            })
+
+            if (li.retrieve('tuple') in board.overlays) {
+                board.overlays[li.retrieve('tuple')].unpack(function(type, ghost, label) {
+                    if (type != '') li.addClass(type)
+                    if (ghost != 0) li.addClass('ghost_' + ghost)
+                    if (label != '') li.set('data-label', label)
+                })
+            }
+
+            if (li.hasClass('sign_' + sign)) continue
+
+            for (var i = -1; i <= 1; i++) {
+                if (li.hasClass('sign_' + i)) li.removeClass('sign_' + i)
+            }
+
+            li.addClass('sign_' + sign)
+                .getElement('img').set('src', '../img/goban/stone_' + sign + '.png')
+        }
+    }
+}
+
+function getScoringMethod() {
+    return $$('#score .method .territory')[0].hasClass('current') ? 'territory' : 'area'
+}
+
+function setScoringMethod(method) {
+    $$('#score .method li').removeClass('current')
+    $$('#score .method .' + method).addClass('current')
+    $$('#score tr > *').addClass('disabled')
+    $$('#score table .' + method).removeClass('disabled')
+
+    setting.set('scoring.method', method)
+
+    // Update UI
+    for (var sign = -1; sign <= 1; sign += 2) {
+        var tr = $$('#score tbody tr' + (sign < 0 ? ':last-child' : ''))[0]
+        var tds = tr.getElements('td')
+
+        tds[4].set('text', 0)
+
+        for (var i = 0; i <= 3; i++) {
+            if (tds[i].hasClass('disabled') || isNaN(tds[i].get('text').toFloat())) continue
+            tds[4].set('text', tds[4].get('text').toFloat() + tds[i].get('text').toFloat())
+        }
+    }
+}
+
+/**
+ * Methods
+ */
+
 function loadSettings() {
     if (setting.get('view.fuzzy_stone_placement'))
         $('goban').addClass('fuzzy')
