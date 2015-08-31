5b6083be0b6deaa3ddebac6cef9b8e2c14776cf9
Yichuan Shen <shen.yichuan@gmail.com>
2015-08-31 22:21:05 +0200

    Add helper.showMessageBox

--- module/helper.js
+++ module/helper.js
@@ -1,5 +1,18 @@
 var crypto = require('crypto')
+var remote = require('remote')
+var dialog = remote.require('dialog')
 
 exports.md5 = function(str) {
     return crypto.createHash('md5').update(str).digest('hex')
+}
+
+exports.showMessageBox = function(message, type, buttons, cancelId) {
+    dialog.showMessageBox(remote.getCurrentWindow(), {
+        'type': type,
+        'buttons': buttons,
+        'title': app.getName(),
+        'message': message,
+        'cancelId': cancelId,
+        'noLink': true
+    })
 }
