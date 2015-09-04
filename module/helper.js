971afde02e725280c8132a6a69c5e1c1090426cf
Yichuan Shen <shen.yichuan@gmail.com>
2015-09-05 00:48:51 +0200

    Revert "Don't use Mootools Array.pick"

--- module/helper.js
+++ module/helper.js
@@ -4,13 +4,6 @@ exports.md5 = function(str) {
     return crypto.createHash('md5').update(str).digest('hex')
 }
 
-exports.pick = function(array) {
-    for (var i = 0; i < array.length; i++){
-        if (array[i] != null) return array[i];
-    }
-    return null;
-}
-
 exports.roundEven = function(float) {
     var value = Math.round(float)
     return value % 2 == 0 ? value : value - 1
