6f2e9330c09be7b611d4820adda3a283749d8862
Yichuan Shen <shen.yichuan@gmail.com>
2015-09-07 17:49:34 +0200

    Write response extractor

--- module/gtp/controller.js
+++ module/gtp/controller.js
@@ -1,5 +1,6 @@
 var child_process = require('child_process')
 var events = require('events')
+var Response = require('./response')
 
 var Controller = function(command, args) {
     var self = this
@@ -9,7 +10,17 @@ var Controller = function(command, args) {
     self.process = child_process.spawn(command, args)
 
     self.process.stdout.on('data', function(data) {
-        self.emit('data', data)
+        self._buffer += (data + '').replace(/\r/g, '').replace(/#.*?\n/g, '').replace(/\t/g, ' ')
+
+        var start = self._buffer.indexOf('\n\n')
+
+        while (start != -1) {
+            var response = Response.parse(self._buffer.substr(0, start))
+            self._buffer = self._buffer.substr(start + 2)
+
+            start = self._buffer.indexOf('\n\n')
+            self.emit('response', response)
+        }
     })
 }
 
