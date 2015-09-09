ad3272dff55f4fe138ccb390eaf83d697d110c5b
Yichuan Shen <shen.yichuan@gmail.com>
2015-09-09 10:42:02 +0200

    Impement array sort

--- module/fuzzyfinder.js
+++ module/fuzzyfinder.js
@@ -10,10 +10,17 @@ exports.find = function(needle, haystack) {
         return [info[0].length, info.index, info.input]
     }).filter(function(s) { return s != null })
 
-    result.sort()
+    result.sort(arraySort)
     return result.map(function(x) { return x[2] })
 }
 
 function removeSpecialChars(input) {
     return input.replace(/[.*+?^${}()|[\]\\]/g, '')
+}
+
+function arraySort(a, b) {
+    if (a.length == 0 || b.length == 0) return a.length - b.length
+    if (a[0] < b[0]) return -1
+    else if (a[0] > b[0]) return 1
+    return arraySort(a.slice(1), b.slice(1))
 }
