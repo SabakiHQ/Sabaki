f2043e83f318df88b332d2fedc7d6006e230d535
Yichuan Shen <shen.yichuan@gmail.com>
2016-02-25 17:27:42 +0100

    Revert "Fix gametree.navigate"
    This reverts commit c64213fe7fbee67bf7e635d04a36fcf1ce08941f.

--- view/index.js
+++ view/index.js
@@ -756,7 +756,7 @@ function useTool(vertex, event) {
     }
 
     if (tool.indexOf('stone') != -1) {
-        if (gametree.navigate(tree, index, 1)[0] != null) {
+        if ('B' in node || 'W' in node) {
             // New variation needed
             var splitted = gametree.splitTree(tree, index)
 
