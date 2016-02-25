927f7f1985756865d6e15b7d833d2a55722c4afe
Yichuan Shen <shen.yichuan@gmail.com>
2016-02-25 19:21:15 +0100

    Fix typo

--- modules/setting.js
+++ modules/setting.js
@@ -69,7 +69,7 @@ var defaults = {
     'view.show_comments': false,
     'view.show_coordinates': false,
     'view.show_graph': false,
-    'view.show_show_next_moves': true,
+    'view.show_next_moves': true,
     'view.leftsidebar_width': 250,
     'view.leftsidebar_minwidth': 100,
     'view.sidebar_width': 200,
