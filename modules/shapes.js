132d28f99ebf2b9f8480627259c55fee366a5503
Yichuan Shen <shen.yichuan@gmail.com>
2016-02-27 21:39:18 +0100

    Revert shapes module

--- modules/shapes.js
+++ modules/shapes.js
@@ -10,39 +10,18 @@ var context = typeof module != 'undefined' ? module.exports : (window.shapes = {
 
 context.parseFile = function(filename) {
     var tree = sgf.parseFile(filename).subtrees[0]
-    var result = []
+    var result = {}
 
     for (var i = 0; i < tree.subtrees.length; i++) {
-        var node = tree.subtrees[i].nodes[0]
+        var node = sgf.addBoard(tree.subtrees[i], 0).nodes[0]
         var name = node.N[0]
         var area = node.CR.map(function(x) { return sgf.point2vertex(x) })
-        var anchor = [3, 3]
+        var board = node.board
 
-        var calculateDiff = function(xs) {
-            var vs = xs.map(sgf.point2vertex)
-            var result = []
-
-            for (var j = 0; j < vs.length; j++) {
-                var anchor = vs[j]
-                var diffs = []
-
-                for (var k = 0; k < vs.length; k++) {
-                    if (k == j) continue
-                    diffs.push([vs[k][0] - anchor[0], vs[k][1] - anchor[1]])
-                }
-
-                result.push(diffs)
-            }
-
-            return result
-        }
-
-        result.push({
-            name: name,
+        result[name] = {
             area: area,
-            "1": 'AB' in node ? calculateDiff(node.AB) : [],
-            "-1": 'AW' in node ? calculateDiff(node.AW) : []
-        })
+            board: board
+        }
     }
 
     return result
