29182c2185d70f9adc32e2760bdadbcdb71aa5b5
Yichuan Shen <shen.yichuan@gmail.com>
2016-02-27 17:06:46 +0100

    Remove Board.getSurroundings()

--- modules/board.js
+++ modules/board.js
@@ -98,34 +98,6 @@ Board.prototype = {
         return result
     },
 
-    getSurroundings: function(vertex) {
-        if (!this.hasVertex(vertex)) return []
-
-        var self = this
-        var result = self.getNeighborhood(vertex)
-        var stones = [vertex]
-
-        while (true) {
-            var newstones = result.filter(function(v) {
-                return self.arrangement[v] != 0
-                    && !stones.some(function(w) { return w[0] == v[0] && w[1] == v[1] })
-            })
-
-            if (newstones.length == 0) return result
-
-            newstones.forEach(function(v) {
-                if (stones.some(function(w) { return w[0] == v[0] && w[1] == v[1] })) return
-
-                self.getNeighborhood(v).forEach(function(n) {
-                    if (result.some(function(w) { return w[0] == n[0] && w[1] == n[1] })) return
-                    result.push(n)
-                })
-
-                stones.push(v)
-            })
-        }
-    },
-
     getConnectedComponent: function(vertex, colors, result) {
         if (!this.hasVertex(vertex)) return []
         if (!result) result = [vertex]
