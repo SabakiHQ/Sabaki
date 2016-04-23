6069acb2aee54c4572d59529cd8874da24a46d92
Yichuan Shen <shen.yichuan@gmail.com>
2016-04-23 15:55:44 +0200

    Use app.getName()

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ var isReady = false
 function newWindow(path) {
     var window = new BrowserWindow({
         icon: process.platform == 'linux' ? __dirname + '/logo.png' : null,
-        title: 'Sabaki',
+        title: app.getName(),
         useContentSize: true,
         width: setting.get('window.width'),
         height: setting.get('window.height'),
