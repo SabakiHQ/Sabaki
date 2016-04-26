344f53eae0f13ae28373b071c30b9735fc6c82f3
Yichuan Shen <shen.yichuan@gmail.com>
2016-04-27 00:40:43 +0200

    Add stone image paths settings

--- view/index.js
+++ view/index.js
@@ -226,7 +226,7 @@ function setBoard(board) {
             }
 
             li.addClass('sign_' + sign).getElement('img')
-                .set('src', '../img/goban/stone_' + sign + '.svg')
+                .set('src', setting.get('board.stone_image_' + sign))
         }
     }
 
