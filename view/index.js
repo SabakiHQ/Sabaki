aaf06b504bc339db054d31cbb94969913aa5b628
Yichuan Shen <shen.yichuan@gmail.com>
2016-05-09 23:53:43 +0200

    Add FF[4] to empty game tree

--- view/index.js
+++ view/index.js
@@ -349,7 +349,7 @@ function setHotspot(bookmark) {
 }
 
 function getEmptyGameTree() {
-    var buffer = ';GM[1]AP[' + app.getName() + ':' + app.getVersion() + ']'
+    var buffer = ';GM[1]FF[4]AP[' + app.getName() + ':' + app.getVersion() + ']'
     buffer += 'CA[UTF-8]KM[' + setting.get('game.default_komi')
         + ']SZ[' + setting.get('game.default_board_size') + ']'
 
