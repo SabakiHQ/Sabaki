407d460dbe340dfc728280d6771627af18c811c7
Yichuan Shen <shen.yichuan@gmail.com>
2016-06-08 00:32:18 +0200

    Disable debug mode

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ function newWindow(path) {
 
     window.loadURL('file://' + __dirname + '/view/index.html')
 
-    // if (setting.get('debug.dev_tools'))
+    if (setting.get('debug.dev_tools'))
         window.toggleDevTools()
 
     return window
