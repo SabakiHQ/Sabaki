61f134036a8b3354e4159fb5a6ce07b706cec677
Yichuan Shen <shen.yichuan@gmail.com>
2016-08-10 16:03:05 +0200

    Add updater module

---
+++ modules/updater.js
@@ -0,0 +1,27 @@
+const {app} = require('electron')
+const dns = require('dns')
+const https = require('https')
+
+exports.check = function(repo, callback) {
+    let url = `https://github.com/${repo}/releases/latest`
+
+    // Check internet connection first
+    dns.lookup('github.com', err => {
+        if (err) return callback(err)
+
+        https.get(url, response => {
+            let content = ''
+
+            response.on('data', chunk => {
+                content += chunk
+            })
+
+            response.on('end', () => {
+                let hasUpdates = content.indexOf('/tag/v' + app.getVersion()) == -1
+                callback(null, hasUpdates, url)
+            })
+        }).on('error', err => {
+            callback(err)
+        })
+    })
+}
