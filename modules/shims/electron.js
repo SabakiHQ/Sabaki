85d79e42e9a65615f8e0d74f2e8a18bdc1fa4766
Yichuan Shen <shen.yichuan@gmail.com>
2017-04-23 16:32:42 +0200

    Shim remote.getCurrentWindow()

--- modules/shims/electron.js
+++ modules/shims/electron.js
@@ -1,12 +1,29 @@
 const path = require('path')
+const helper = require('../helper')
+
+const BrowserWindow = {
+    show: helper.noop,
+    close: helper.noop,
+    on: helper.noop,
+    isMaximized: false,
+    isMinimized: false,
+    isFullScreen: false,
+    setFullScreen: helper.noop,
+    setMenuBarVisibility: helper.noop,
+    setAutoHideMenuBar: helper.noop,
+    setProgressBar: helper.noop,
+    setContentSize: helper.noop,
+    getContentSize: [],
+    webContents: {setAudioMuted: helper.noop}
+}
 
 module.exports = {
-    ipcRenderer: {},
-    clipboard: {},
-    shell: {},
+    ipcRenderer: {on: helper.noop, send: helper.noop},
+    clipboard: {readText: helper.noop, writeText: helper.noop},
+    shell: {openExternal: helper.noop},
     remote: {
         require: x => x === './modules/setting' ? require('../setting') : {},
-        getCurrentWindow: () => null,
+        getCurrentWindow: () => BrowserWindow,
         app: {getName: () => 'Sabaki', getVersion: () => 'web', getPath: () => ''},
         dialog: {},
         Menu: {}
