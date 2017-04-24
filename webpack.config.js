ed5c7a940dbc9582289e57b08beb9559643b6db0
Yichuan Shen <shen.yichuan@gmail.com>
2017-04-24 02:36:00 +0200

    Reformat

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,44 @@
 const path = require('path')
 
-module.exports = function(env) {
-    return {
-        entry: './components/App.js',
+module.exports = () => ({
+    entry: './components/App.js',
 
-        output: {
-            filename: 'bundle.js',
-            path: __dirname
-        },
+    output: {
+        filename: 'bundle.js',
+        path: __dirname
+    },
 
-        devtool: 'source-map',
+    devtool: 'source-map',
 
-        module: {
-            loaders: [
-                {
-                    loader: 'babel-loader',
-                    exclude: /node_modules/,
-                    test: /\.js$/,
-                    query: {
-                        presets: ['es2015']
-                    }
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                test: /\.js$/,
+                query: {
+                    presets: ['es2015']
                 }
-            ]
-        },
-
-        resolve: {
-            alias: {
-                'fs$': path.join(__dirname, 'modules/shims/fs'),
-                'electron$': path.join(__dirname, 'modules/shims/electron'),
-                'iconv-lite$': path.join(__dirname, 'modules/shims/iconv-lite'),
-                'jschardet$': path.join(__dirname, 'modules/shims/jschardet'),
-                'moment$': path.join(__dirname, 'modules/shims/empty'),
-                'argv-split$': path.join(__dirname, 'modules/shims/empty'),
-                '../modules/gtp$': path.join(__dirname, 'modules/shims/empty'),
-                '../data/menu$': path.join(__dirname, 'modules/shims/empty'),
-
-                './LeftSidebar$': path.join(__dirname, 'modules/shims/noop'),
-                './drawers/PreferencesDrawer$': path.join(__dirname, 'modules/shims/noop'),
-                './drawers/CleanMarkupDrawer$': path.join(__dirname, 'modules/shims/noop'),
-                './bars/AutoplayBar$': path.join(__dirname, 'modules/shims/noop'),
-                './bars/GuessBar$': path.join(__dirname, 'modules/shims/noop')
             }
+        ]
+    },
+
+    resolve: {
+        alias: {
+            'fs$': path.join(__dirname, 'modules/shims/fs'),
+            'electron$': path.join(__dirname, 'modules/shims/electron'),
+            'iconv-lite$': path.join(__dirname, 'modules/shims/iconv-lite'),
+            'jschardet$': path.join(__dirname, 'modules/shims/jschardet'),
+            'moment$': path.join(__dirname, 'modules/shims/empty'),
+            'argv-split$': path.join(__dirname, 'modules/shims/empty'),
+            '../modules/gtp$': path.join(__dirname, 'modules/shims/empty'),
+            '../data/menu$': path.join(__dirname, 'modules/shims/empty'),
+
+            './LeftSidebar$': path.join(__dirname, 'modules/shims/noop'),
+            './drawers/PreferencesDrawer$': path.join(__dirname, 'modules/shims/noop'),
+            './drawers/CleanMarkupDrawer$': path.join(__dirname, 'modules/shims/noop'),
+            './bars/AutoplayBar$': path.join(__dirname, 'modules/shims/noop'),
+            './bars/GuessBar$': path.join(__dirname, 'modules/shims/noop')
         }
     }
-}
+})
