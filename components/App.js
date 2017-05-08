778eab621c100d19691b8cdbee92cd6cc839cc8a
Yichuan Shen <shen.yichuan@gmail.com>
2017-05-08 15:52:03 +0200

    Fix formatting

--- components/App.js
+++ components/App.js
@@ -88,7 +88,7 @@ class App extends Component {
             // Drawers
 
             preferencesTab: 'general',
-            preferencesTab: 'visual',
+
             // Input Box
 
             showInputBox: false,
@@ -2136,59 +2136,62 @@ class App extends Component {
 
         state = Object.assign(state, this.inferredState)
 
-         return h('section',
-                    {
-                        class: classNames({
-                            leftsidebar: state.showLeftSidebar,
-                            sidebar: state.showSidebar,
-                            [state.mode]: true
-                    })
-                },
-                setting.get('custom_theme') !== 'defaulttheme' ?
-                h('link', {
+        return h('section',
+            {
+                class: classNames({
+                    leftsidebar: state.showLeftSidebar,
+                    sidebar: state.showSidebar,
+                    [state.mode]: true
+                })
+            },
+
+            setting.get('themes.custom_theme') !== ''
+            ? h('link', {
+                rel: 'stylesheet',
+                type: 'text/css',
+                href: path.join(setting.get('themes.custom_theme'), 'styles.css')
+            }) : null,
+
+            h('link',
+                {
                     rel: 'stylesheet',
                     type: 'text/css',
-                    href: path.join(setting.get('custom_theme'), 'styles.css')
-                }):null,
+                    href: setting.stylesPath
+                },
 
-                h('link', 
-                    {
-                        rel: 'stylesheet', 
-                        type: 'text/css', 
-                        href: setting.stylesPath
-                    },
-                    setting.get('custom_blackstones') !== "" ?
-                    h('style', {}, `
-                        .goban li.sign_1 .stone img 
-                        { background-image: url(${setting.get('custom_blackstones')[0]});}
-                        // .goban li.sign_1.random_1 .stone img 
-                        // { background-image: url(${setting.get('custom_blackstones')[1]});}
-                        // .goban li.sign_1.random_2 .stone img 
-                        // { background-image: url(${setting.get('custom_blackstones')[2]});}
-                        // .goban li.sign_1.random_3 .stone img 
-                        // { background-image: url(${setting.get('custom_blackstones')[3]});}
-                        // .goban li.sign_1.random_4 .stone img 
-                        // { background-image: url(${setting.get('custom_blackstones')[4]});}
-                    `):null,
-                    setting.get('custom_whitestones') !== "" ?
-                    h('style', {}, `
-                        .goban li.sign_-1 .stone img 
-                        { background-image: url(${setting.get('custom_whitestones')[0]});}
-                        // .goban li.sign_-1.random_1 .stone img 
-                        // { background-image: url(${setting.get('custom_whitestones')[1]});}
-                        // .goban li.sign_-1.random_2 .stone img 
-                        // { background-image: url(${setting.get('custom_whitestones')[2]});}
-                        // .goban li.sign_-1.random_3 .stone img 
-                        // { background-image: url(${setting.get('custom_whitestones')[3]});}
-                        // .goban li.sign_-1.random_4 .stone img 
-                        // { background-image: url(${setting.get('custom_whitestones')[4]});}
-                    `):null,
-                    setting.get('custom_background') !== "" ?
-                    h('style', {}, `
-                        main 
-                        { background-image: url(${setting.get('custom_background')});}
-                    `):null
-                ),
+                setting.get('themes.custom_blackstones') !== ''
+                ? h('style', {}, `
+                    .goban li.sign_1 .stone img
+                    { background-image: url(${setting.get('themes.custom_blackstones')[0]});}
+                    // .goban li.sign_1.random_1 .stone img
+                    // { background-image: url(${setting.get('themes.custom_blackstones')[1]});}
+                    // .goban li.sign_1.random_2 .stone img
+                    // { background-image: url(${setting.get('themes.custom_blackstones')[2]});}
+                    // .goban li.sign_1.random_3 .stone img
+                    // { background-image: url(${setting.get('themes.custom_blackstones')[3]});}
+                    // .goban li.sign_1.random_4 .stone img
+                    // { background-image: url(${setting.get('themes.custom_blackstones')[4]});}
+                `) : null,
+
+                setting.get('themes.custom_whitestones') !== ''
+                ? h('style', {}, `
+                    .goban li.sign_-1 .stone img
+                    { background-image: url(${setting.get('themes.custom_whitestones')[0]});}
+                    // .goban li.sign_-1.random_1 .stone img
+                    // { background-image: url(${setting.get('themes.custom_whitestones')[1]});}
+                    // .goban li.sign_-1.random_2 .stone img
+                    // { background-image: url(${setting.get('themes.custom_whitestones')[2]});}
+                    // .goban li.sign_-1.random_3 .stone img
+                    // { background-image: url(${setting.get('themes.custom_whitestones')[3]});}
+                    // .goban li.sign_-1.random_4 .stone img
+                    // { background-image: url(${setting.get('themes.custom_whitestones')[4]});}
+                `) : null,
+
+                setting.get('themes.custom_background') !== ''
+                ? h('style', {},
+                    `main { background-image: url(${setting.get('themes.custom_background')});}`
+                ) : null
+            ),
 
             h(MainView, state),
             h(LeftSidebar, state),
--- components/drawers/PreferencesDrawer.js
+++ components/drawers/PreferencesDrawer.js
@@ -8,7 +8,7 @@ const Drawer = require('./Drawer')
 const dialog = require('../../modules/dialog')
 const helper = require('../../modules/helper')
 const setting = remote.require('./modules/setting')
-const originalFs = require('original-fs')
+const fs = require('original-fs')
 
 class PreferencesItem extends Component {
     constructor() {
@@ -142,8 +142,7 @@ class GeneralTab extends Component {
     }
 }
 
-
-class VisualTab extends Component {
+class ThemesTab extends Component {
     constructor() {
         super()
 
@@ -155,59 +154,55 @@ class VisualTab extends Component {
         this.handleBrowseButtonClick = evt => {
             let element = evt.currentTarget
             dialog.showOpenDialog({
-                    properties: ['openFile', 'multiSelections'],
-                }, 
-                ({result}) => {
-                    if (!result || result.length === 0) return
-                    setting.set(element.name, result)
-                }
-            )
+                properties: ['openFile', 'multiSelections'],
+            }, ({result}) => {
+                if (!result || result.length === 0) return
+                setting.set(element.name, result)
+            })
         }
 
         this.handleAddButtonClick = evt => {
             evt.preventDefault()
 
             dialog.showOpenDialog({
-                    properties: ['openFile'],
-                },
-                ({result}) => {
-                    if (!result || result.length === 0) return
-                    let {join , basename} = require('path')
-    
-                    let filename = basename(result[0])
-                    let folder = setting.userDataDirectory
-                    let themepath = join(folder,filename)
-                    let themes = setting.get('themes.list')
-                    // This needs to copy a file from result to the userData directory
-                    // What is the right way to do this? 
-                    originalFs.createReadStream(result[0]).pipe(originalFs.createWriteStream(themepath))
-                    themes.unshift({filename, themepath})
-                    setting.set('themes.list', themes)
-                }
-            )
+                properties: ['openFile'],
+            }, ({result}) => {
+                if (!result || result.length === 0) return
+                let {join , basename} = require('path')
+
+                let filename = basename(result[0])
+                let folder = setting.userDataDirectory
+                let themepath = join(folder,filename)
+                let themes = setting.get('themes.list')
+                // This needs to copy a file from result to the userData directory
+                // What is the right way to do this?
+                fs.createReadStream(result[0]).pipe(fs.createWriteStream(themepath))
+                themes.unshift({filename, themepath})
+                setting.set('themes.list', themes)
+            })
         }
 
-        this.handleThemeChange= evt => {
+        this.handleThemeChange = evt => {
             let element = evt.currentTarget
-            setting.set('custom_theme', element.value)
+            setting.set('themes.custom_theme', element.value)
         }
     }
 
     render() {
-        return h('div', {class: 'visual'},
+        return h('div', {class: 'themes'},
             h('p', {},
                 h('input', {
                     type: 'text',
                     placeholder: 'path/to/WhiteStones.png',
-                    value: setting.get('custom_whitestones'),
+                    value: setting.get('themes.custom_whitestones'),
                     name: 'whitestones',
-                    id: 'custom_whitestones',
+                    id: 'themes.custom_whitestones',
                     onChange: this.handlePathChange
                 }),
-                h('a', 
+                h('a',
                     {
                         class: 'browse',
-                        name: 'custom_whitestones',
+                        name: 'themes.custom_whitestones',
                         onClick: this.handleBrowseButtonClick
                     },
                     h('img', {
@@ -216,19 +211,20 @@ class VisualTab extends Component {
                         height: 14
                     })
                 )
-             ),
+            ),
+
             h('p', {},
                 h('input', {
                     type: 'text',
                     placeholder: 'path/to/BlackStones.png',
-                    id: 'custom_blackstones',
-                    value: setting.get('custom_blackstones'),
+                    id: 'themes.custom_blackstones',
+                    value: setting.get('themes.custom_blackstones'),
                     onChange: this.handlePathChange
                 }),
                 h('a',
                     {
                         class: 'browse',
-                        name: 'custom_blackstones',
+                        name: 'themes.custom_blackstones',
                         onClick: this.handleBrowseButtonClick
                     },
                     h('img', {
@@ -238,18 +234,19 @@ class VisualTab extends Component {
                     })
                 )
             ),
+
             h('p', {},
                 h('input', {
                     type: 'text',
                     placeholder: 'path/to/Background.png',
-                    id: 'custom_background',
-                    value: setting.get('custom_background'),
+                    id: 'themes.custom_background',
+                    value: setting.get('themes.custom_background'),
                     onChange: this.handlePathChange
                 }),
                 h('a',
                     {
                         class: 'browse',
-                        name: 'custom_background',
+                        name: 'themes.custom_background',
                         onClick: this.handleBrowseButtonClick
                     },
                     h('img', {
@@ -259,32 +256,28 @@ class VisualTab extends Component {
                     })
                 )
             ),
-            h('p', {}, 
+
+            h('p', {},
                 h('label', {}, 'Main Theme: ',
                     h('select',
                         {
-                            id: 'custom_theme',
+                            id: 'themes.custom_theme',
                             onChange: this.handleThemeChange
                         },
                         h('option',
                             {value: 'defaulttheme'},
                             'Default'
                         ),
-                        setting.get('themes.list').map(
-                            ({filename, themepath}) =>
-                            h('option', 
-                                {value: themepath},
-                                filename
-                            ) 
-                        )
+                        setting.get('themes.list').map(({filename, themepath}) => h('option',
+                            {value: themepath},
+                            filename
+                        ))
                     )
                 )
             ),
+
             h('p', {},
-                h('button',
-                    {onClick: this.handleAddButtonClick},
-                    'Add'
-                )
+                h('button', {onClick: this.handleAddButtonClick}, 'Add')
             )
         )
     }
@@ -441,7 +434,7 @@ class PreferencesDrawer extends Component {
         }
 
         this.handleTabClick = evt => {
-            let tabs = ['general', 'visual', 'engines']
+            let tabs = ['general', 'themes', 'engines']
             let tab = tabs.find(x => evt.currentTarget.classList.contains(x))
 
             sabaki.setState({preferencesTab: tab})
@@ -474,7 +467,8 @@ class PreferencesDrawer extends Component {
     }
 
     render({show, tab, engines, graphGridSize}) {
-        return h(Drawer, {
+        return h(Drawer,
+            {
                 type: 'preferences',
                 show
             },
@@ -489,12 +483,12 @@ class PreferencesDrawer extends Component {
                 ),
                 h('li',
                     {
-                        class: classNames({visual: true, current: tab === 'visual'}),
+                        class: classNames({themes: true, current: tab === 'themes'}),
                         onClick: this.handleTabClick
                     },
 
                     h('a', {href: '#'}, 'Themes')
-                ),   
+                ),
                 h('li',
                     {
                         class: classNames({engines: true, current: tab === 'engines'}),
@@ -507,7 +501,7 @@ class PreferencesDrawer extends Component {
 
             h('form', {class: tab},
                 h(GeneralTab, {graphGridSize}),
-                h(VisualTab, {}),
+                h(ThemesTab),
                 h(EnginesTab, {engines}),
                 h('p', {},
                     h('button', {onClick: this.handleCloseButtonClick}, 'Close')
