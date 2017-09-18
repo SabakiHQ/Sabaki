d611fc20cb32685adaba425fd07b45f28ee55d25
Yichuan Shen <shen.yichuan@gmail.com>
2017-09-18 13:43:04 +0200

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -27,26 +27,26 @@ module.exports = () => ({
 
     resolve: {
         alias: {
-            'preact$': path.join(__dirname, 'node_modules/preact/dist/preact.min'),
-            'fs$': path.join(__dirname, 'modules/shims/fs'),
-            'electron$': path.join(__dirname, 'modules/shims/electron'),
-            'buffer$': path.join(__dirname, 'modules/shims/buffer'),
-            'iconv-lite$': path.join(__dirname, 'modules/shims/iconv-lite'),
-            'jschardet$': path.join(__dirname, 'modules/shims/jschardet'),
-            'moment$': emptyPath,
-            'uuid$': emptyPath,
-            'recursive-copy$': emptyPath,
-            'rimraf$': emptyPath,
-            'argv-split$': emptyPath,
-            '../modules/gtp$': emptyPath,
-            '../data/menu$': emptyPath,
+            'preact': path.join(__dirname, 'node_modules/preact/dist/preact.min'),
+            'fs': path.join(__dirname, 'modules/shims/fs'),
+            'electron': path.join(__dirname, 'modules/shims/electron'),
+            'buffer': path.join(__dirname, 'modules/shims/buffer'),
+            'iconv-lite': path.join(__dirname, 'modules/shims/iconv-lite'),
+            'jschardet': path.join(__dirname, 'modules/shims/jschardet'),
+            'moment': emptyPath,
+            'uuid': emptyPath,
+            'recursive-copy': emptyPath,
+            'rimraf': emptyPath,
+            'argv-split': emptyPath,
+            '../modules/gtp': emptyPath,
+            '../data/menu': emptyPath,
 
-            './ThemeManager$': noopPath,
-            './LeftSidebar$': noopPath,
-            './drawers/PreferencesDrawer$': noopPath,
-            './drawers/CleanMarkupDrawer$': noopPath,
-            './bars/AutoplayBar$': noopPath,
-            './bars/GuessBar$': noopPath
+            './ThemeManager': noopPath,
+            './LeftSidebar': noopPath,
+            './drawers/PreferencesDrawer': noopPath,
+            './drawers/CleanMarkupDrawer': noopPath,
+            './bars/AutoplayBar': noopPath,
+            './bars/GuessBar': noopPath
         }
     }
 })
