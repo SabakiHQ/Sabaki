33134180abbc1a77cbab12119e77f79538db05aa
Yichuan Shen <shen.yichuan@gmail.com>
2018-06-09 13:29:35 +0200

    Design UI

--- src/components/drawers/AdvancedPropertiesDrawer.js
+++ src/components/drawers/AdvancedPropertiesDrawer.js
@@ -4,6 +4,40 @@ const Drawer = require('./Drawer')
 
 const blockedProperties = ['B', 'W', 'C', 'SZ']
 
+class PropertyItem extends Component {
+    shouldComponentUpdate({property, index, value, disabled}) {
+        return property !== this.props.property
+            || index !== this.props.index
+            || value !== this.props.value
+            || disabled !== this.props.disabled
+    }
+
+    render({property, index, value, disabled}) {
+        return h('tr', {},
+            h('th',
+                {
+                    style: {
+                        width: +(index != null) * 3 + property.length
+                    }
+                },
+
+                index == null ? property : [property, h('em', {}, `[${index}]`)]
+            ),
+            h('td', {},
+                h('input', {
+                    value,
+                    disabled
+                })
+            ),
+            h('td', {class: 'action'},
+                h('a', {class: 'remove', title: 'Remove', href: '#'},
+                    h('img', {src: './node_modules/octicons/build/svg/x.svg'})
+                )
+            )
+        )
+    }
+}
+
 class AdvancedPropertiesDrawer extends Component {
     constructor(props) {
         super(props)
@@ -21,7 +55,7 @@ class AdvancedPropertiesDrawer extends Component {
     render({treePosition, show}) {
         let [tree, index] = treePosition
         let node = tree.nodes[index]
-        let properties = Object.keys(node).filter(x => x.toUpperCase() === x)
+        let properties = Object.keys(node).filter(x => x.toUpperCase() === x).sort()
 
         return h(Drawer,
             {
@@ -30,6 +64,21 @@ class AdvancedPropertiesDrawer extends Component {
             },
 
             h('form', {},
+                h('div', {class: 'properties-list'},
+                    h('table', {},
+                        properties.map(property =>
+                            node[property].map((value, i) =>
+                                h(PropertyItem, {
+                                    property,
+                                    value,
+                                    index: node[property].length === 1 ? null : i,
+                                    disabled: blockedProperties.includes(property)
+                                })
+                            )
+                        )
+                    )
+                ),
+
                 h('p', {},
                     h('button', {onClick: this.handleCloseButtonClick}, 'Close')
                 )
