0d273fe82f9c1edf975c399fb5895dcf05da5a35
Yichuan Shen <shen.yichuan@gmail.com>
2018-09-24 22:42:56 +0200

    Add custom board img setting & adapt colors to custom imgs

--- src/components/ThemeManager.js
+++ src/components/ThemeManager.js
@@ -1,8 +1,24 @@
-const path = require('path')
+const {join} = require('path')
 const {remote} = require('electron')
 const {h, Component} = require('preact')
+const ColorThief = require('@mariotacke/color-thief')
 
 const setting = remote.require('./setting')
+const colorThief = new ColorThief()
+
+async function getColorFromPath(path) {
+    try {
+        let img = new Image()
+        img.src = path
+        await new Promise(resolve => img.addEventListener('load', resolve))
+
+        return colorThief.getColor(img)
+    } catch (err) {}
+}
+
+function getForeground([r, g, b]) {
+    return Math.max(r, g, b) < 255 / 2 ? 'white' : 'black'
+}
 
 class ThemeManager extends Component {
     constructor() {
@@ -19,6 +35,11 @@ class ThemeManager extends Component {
             || state.whiteStonePath !== this.state.whiteStonePath
             || state.boardPath !== this.state.boardPath
             || state.backgroundPath !== this.state.backgroundPath
+            || state.boardBackground !== this.state.boardBackground
+            || state.blackStoneBackground !== this.state.blackStoneBackground
+            || state.blackStoneForeground !== this.state.blackStoneForeground
+            || state.whiteStoneBackground !== this.state.whiteStoneBackground
+            || state.whiteStoneForeground !== this.state.whiteStoneForeground
     }
 
     updateSettingState(key) {
@@ -40,7 +61,50 @@ class ThemeManager extends Component {
         }
     }
 
-    render(_, {currentThemeId, blackStonePath, whiteStonePath, boardPath, backgroundPath}) {
+    componentDidMount() {
+        this.componentDidUpdate(null, {})
+    }
+
+    componentDidUpdate(_, prevState) {
+        let {blackStonePath, whiteStonePath, boardPath} = this.state
+
+        if (boardPath != null && prevState.boardPath !== boardPath) {
+            getColorFromPath(boardPath).then(color =>
+                this.setState({boardBackground: color ? `rgb(${color.join(',')})` : undefined})
+            )
+        }
+
+        if (blackStonePath != null && prevState.blackStonePath !== blackStonePath) {
+            getColorFromPath(blackStonePath).then(color =>
+                this.setState({
+                    blackStoneBackground: color ? `rgb(${color.join(',')})` : undefined,
+                    blackStoneForeground: color ? getForeground(color) : undefined
+                })
+            )
+        }
+
+        if (whiteStonePath != null && prevState.whiteStonePath !== whiteStonePath) {
+            getColorFromPath(whiteStonePath).then(color =>
+                this.setState({
+                    whiteStoneBackground: color ? `rgb(${color.join(',')})` : undefined,
+                    whiteStoneForeground: color ? getForeground(color) : undefined
+                })
+            )
+        }
+    }
+
+    render(_, {
+        currentThemeId,
+        blackStonePath,
+        whiteStonePath,
+        boardPath,
+        backgroundPath,
+        boardBackground = '#EBB55B',
+        blackStoneBackground = 'black',
+        blackStoneForeground = 'white',
+        whiteStoneBackground = 'white',
+        whiteStoneForeground = 'black',
+    }) {
         let currentTheme = setting.getThemes()[currentThemeId]
 
         return h('section', {},
@@ -49,7 +113,7 @@ class ThemeManager extends Component {
             currentTheme != null && h('link', {
                 rel: 'stylesheet',
                 type: 'text/css',
-                href: path.join(currentTheme.path, currentTheme.main || 'styles.css')
+                href: join(currentTheme.path, currentTheme.main || 'styles.css')
             }),
 
             // Custom images
@@ -57,14 +121,23 @@ class ThemeManager extends Component {
             h('style', {},
                 blackStonePath != null && `.shudan-vertex.shudan-sign_1 .shudan-inner {
                     background-image: url('${blackStonePath.replace(/\\/g, '/')}');
+                } .shudan-goban {
+                    --shudan-black-background-color: ${blackStoneBackground};
+                    --shudan-black-foreground-color: ${blackStoneForeground};
                 }`,
 
                 whiteStonePath != null && `.shudan-vertex.shudan-sign_-1 .shudan-inner {
                     background-image: url('${whiteStonePath.replace(/\\/g, '/')}');
+                } .shudan-goban {
+                    --shudan-white-background-color: ${whiteStoneBackground};
+                    --shudan-white-foreground-color: ${whiteStoneForeground};
                 }`,
 
                 boardPath != null && `.shudan-goban {
                     background-image: url('${boardPath.replace(/\\/g, '/')}');
+                    --shudan-board-background-color: ${boardBackground};
+                    --shudan-board-border-color: rgba(33, 24, 9, .2);
+                    --shudan-board-foreground-color: rgba(33, 24, 9, 1);
                 }`,
 
                 backgroundPath != null && `main {
