f9f0af779bd19021791e813b4120f4f5776129a9
Yichuan Shen <shen.yichuan@gmail.com>
2018-11-22 19:26:05 +0100

    Adapt rotation module

--- src/modules/rotation.js
+++ src/modules/rotation.js
@@ -1,5 +1,3 @@
-// TODO
-
 const simple = ['AB', 'AW', 'AE', 'B', 'CR', 'DD', 'MA', 'SL', 'SQ', 'TB', 'TR', 'TW', 'VW', 'W']
 const pointWithText = ['LB']
 const arrowish = ['AR', 'LN']
@@ -17,157 +15,152 @@ const alphaRev = [...alpha].reverse()
 // but is considered lower in the SGF system.
 
 function min(one, two) {
-	// In case exactly one is lowercase, return it...
+    // In case exactly one is lowercase, return it...
 
-	if (one >= 'a' && two <= 'Z') return one
-	if (one <= 'Z' && two >= 'a') return two
+    if (one >= 'a' && two <= 'Z') return one
+    if (one <= 'Z' && two >= 'a') return two
 
-	// Same case...
+    // Same case...
 
-	if (one < two) return one
-	return two
+    if (one < two) return one
+    return two
 }
 
 function max(one, two) {
-	if (one >= 'a' && two <= 'Z') return two
-	if (one <= 'Z' && two >= 'a') return one
-	if (one < two) return two
-	return one
+    if (one >= 'a' && two <= 'Z') return two
+    if (one <= 'Z' && two >= 'a') return one
+    if (one < two) return two
+    return one
 }
 
 exports.rotatePoint = function(point, width, height, anticlockwise) {
-	// returns null on failure; point is something like "aa"
+    // returns null on failure; point is something like "aa"
 
-	if (typeof point !== 'string') return null
-	if (point.length !== 2) return null
+    if (typeof point !== 'string') return null
+    if (point.length !== 2) return null
 
-	if (typeof width !== 'number') return null
-	if (width < 1 || width > 52) return null
+    if (typeof width !== 'number') return null
+    if (width < 1 || width > 52) return null
 
-	if (typeof height !== 'number') return null
-	if (height < 1 || height > 52) return null
+    if (typeof height !== 'number') return null
+    if (height < 1 || height > 52) return null
 
-	let index0 = alpha.indexOf(point[0])
-	let index1 = alpha.indexOf(point[1])
+    let index0 = alpha.indexOf(point[0])
+    let index1 = alpha.indexOf(point[1])
 
-	if (index0 === -1 || index1 === -1) return null
+    if (index0 === -1 || index1 === -1) return null
 
-	if (anticlockwise) {
-		let rev = alphaRev.slice(52 - width)
-		return point[1] + rev[index0]
-	} else {
-		let rev = alphaRev.slice(52 - height)
-		return rev[index1] + point[0]
-	}
+    if (anticlockwise) {
+        let rev = alphaRev.slice(52 - width)
+        return point[1] + rev[index0]
+    } else {
+        let rev = alphaRev.slice(52 - height)
+        return rev[index1] + point[0]
+    }
 }
 
 exports.rotateTwoPoints = function(twopoints, width, height, anticlockwise, isRect) {
-	// returns null on failure; twopoints is something like "aa:cc"
+    // returns null on failure; twopoints is something like "aa:cc"
 
-	if (typeof twopoints !== 'string') return null
-	if (twopoints.length !== 5) return null
-	if (twopoints[2] !== ':') return null
+    if (typeof twopoints !== 'string') return null
+    if (twopoints.length !== 5) return null
+    if (twopoints[2] !== ':') return null
 
-	let first = twopoints.slice(0, 2)
-	let second = twopoints.slice(3, 5)
+    let first = twopoints.slice(0, 2)
+    let second = twopoints.slice(3, 5)
 
-	first = exports.rotatePoint(first, width, height, anticlockwise)
-	second = exports.rotatePoint(second, width, height, anticlockwise)
+    first = exports.rotatePoint(first, width, height, anticlockwise)
+    second = exports.rotatePoint(second, width, height, anticlockwise)
 
-	if (first === null || second === null) {
-		return null
-	}
+    if (first === null || second === null) {
+        return null
+    }
 
-	// For a rectangle (e.g. anything except line / arrow) we
-	// need to make the format topleft : bottomright
+    // For a rectangle (e.g. anything except line / arrow) we
+    // need to make the format topleft : bottomright
 
-	if (isRect) {
-		return min(first[0], second[0])
-			+ min(first[1], second[1])
-			+ ':' + max(first[0], second[0])
-			+ max(first[1], second[1])
-	} else {
-		return first + ':' + second
-	}
+    if (isRect) {
+        return min(first[0], second[0])
+            + min(first[1], second[1])
+            + ':' + max(first[0], second[0])
+            + max(first[1], second[1])
+    } else {
+        return first + ':' + second
+    }
 }
 
 exports.rotateRect = function(twopoints, width, height, anticlockwise) {
-	return exports.rotateTwoPoints(twopoints, width, height, anticlockwise, true)
+    return exports.rotateTwoPoints(twopoints, width, height, anticlockwise, true)
 }
 
 exports.rotateArrow = function(twopoints, width, height, anticlockwise) {
-	return exports.rotateTwoPoints(twopoints, width, height, anticlockwise, false)
+    return exports.rotateTwoPoints(twopoints, width, height, anticlockwise, false)
 }
 
-exports.rotateNode = function(node, width, height, anticlockwise) {
-	// Given a node and a board height, rotates all relevant properties in the node.
-	// Does NOT do anything about the board though.
+exports.rotateTree = function(tree, width, height, anticlockwise) {
+    return tree.mutate(draft => {
+        let rotateNode = node => {
+            // 'simple' cases are either a single point (e.g. "aa") or a rect (e.g. "aa:cc")
 
-	// 'simple' cases are either a single point (e.g. "aa") or a rect (e.g. "aa:cc")
+            for (let key of simple) {
+                if (node.data[key] == null) continue
 
-	for (let k = 0; k < simple.length; k++) {
-		let key = simple[k]
-		if (node[key] == null) continue
+                draft.updateProperty(node.id, key, node.data[key].map(value => {
+                    if (value.length === 2) {
+                        let result = exports.rotatePoint(value, width, height, anticlockwise)
+                        if (result) return result
+                    } else if (value.length === 5 && value[2] === ':') {
+                        let result = exports.rotateRect(value, width, height, anticlockwise)
+                        if (result) return result
+                    }
 
-		let values = node[key]
+                    return value
+                }))
+            }
 
-		for (let v = 0; v < values.length; v++) {
-			let value = values[v]
+            // 'pointWithText' means something like "aa:hi there"
 
-			if (value.length === 2) {
-				let result = exports.rotatePoint(value, width, height, anticlockwise)
-				if (result) {
-					values[v] = result
-				}
-			} else if (value.length === 5 && value[2] === ':') {
-				let result = exports.rotateRect(value, width, height, anticlockwise)
-				if (result) {
-					values[v] = result
-				}
-			}
-		}
-	}
+            for (let key of pointWithText) {
+                if (node.data[key] == null) continue
 
-	// 'pointWithText' means something like "aa:hi there"
+                draft.updateProperty(node.id, key, node.data[key].map(value => {
+                    if (value[2] === ':') {
+                        let point = value.slice(0, 2)
 
-	for (let k = 0; k < pointWithText.length; k++) {
-		let key = pointWithText[k]
-		if (node[key] == null) continue
+                        point = exports.rotatePoint(point, width, height, anticlockwise)
+                        if (point) return point + value.slice(2)
+                    }
 
-		let values = node[key]
+                    return value
+                }))
+            }
 
-		for (let v = 0; v < values.length; v++) {
-			let value = values[v]
+            // 'arrowish' things are formatted like rects, but with no requirement of topleft:bottomright
 
-			if (value[2] === ':') {
-				let point = value.slice(0, 2)
+            for (let key of arrowish) {
+                if (node.data[key] == null) continue
 
-				point = exports.rotatePoint(point, width, height, anticlockwise)
-				if (point) {
-					values[v] = point + value.slice(2)
-				}
-			}
-		}
-	}
+                draft.updateProperty(node.id, key, node.data[key].map(value => {
+                    if (value[2] === ':') {
+                        let result = exports.rotateArrow(value, width, height, anticlockwise)
+                        if (result) return result
+                    }
 
-	// 'arrowish' things are formatted like rects, but with no requirement of topleft:bottomright
+                    return value
+                }))
+            }
 
-	for (let k = 0; k < arrowish.length; k++) {
-		let key = arrowish[k]
-		if (node[key] == null) continue
+            // Rotate children
 
-		let values = node[key]
+            for (let child of node.children) {
+                rotateNode(child)
+            }
+        }
 
-		for (let v = 0; v < values.length; v++) {
-			let value = values[v]
+        rotateNode(draft.root)
 
-			if (value[2] === ':') {
-				let result = exports.rotateArrow(value, width, height, anticlockwise)
-				if (result) {
-					values[v] = result
-				}
-			}
-		}
-	}
+        if (draft.root.data.SZ != null && draft.root.data.SZ.include(':')) {
+            draft.updateProperty(draft.root.id, 'SZ', draft.root.data.SZ.split(':').reverse().join(':'))
+        }
+    })
 }
-
