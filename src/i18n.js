47d3032d4de17382b5ace0bf007f9ac9b86e5417
Yichuan Shen <shen.yichuan@gmail.com>
2019-03-21 23:15:06 +0100

    Combine with main process dolm serialization

--- src/i18n.js
+++ src/i18n.js
@@ -7,24 +7,43 @@ const path = require('path')
 const setting = remote != null ? remote.require('./setting') : customReq('./setting')
 
 const lang = setting.get('app.lang')
-const strings = {}
+
+exports.strings = {}
 
 try {
-    strings = remote != null
+    exports.strings = remote != null
         ? customReq(`./lang/${lang}.js`)
         : customReq(`../lang/${lang}.js`)
 } catch (err) {}
 
-const dolm = require('dolm').load(strings)
+const dolm = require('dolm').load(exports.strings)
 
+exports.usedStrings = dolm.usedStrings
 exports.t = dolm.t
 exports.context = dolm.context
 
 exports.serialize = function() {
+    if (remote != null) {
+        // Merge with dolm serialization result in main process
+
+        let mainResult = remote.require('./i18n').serialize()
+        let mainStrings = eval('(' + mainResult.js + ')')
+        let mainUsedStrings = remote.require('./i18n').usedStrings
+
+        for (context in mainStrings) {
+            if (!(context in exports.strings)) {
+                exports.strings[context] = mainStrings[context]
+                exports.usedStrings[context] = mainUsedStrings[context]
+            }
+        }
+    }
+
     let result = dolm.serialize()
     let js = `module.exports = ${result.js}`
 
-    fs.writeFileSync(path.join(__dirname, remote != null ? '.' : '..', 'lang', `${lang}.js`), js)
+    if (remote != null) {
+        fs.writeFileSync(path.join(__dirname, 'lang', `${lang}.js`), js)
+    }
 
     return result
 }
