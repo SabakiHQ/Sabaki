13756f10bd9b27f6bf9593a176da931853008de3
Yichuan Shen <shen.yichuan@gmail.com>
2019-04-25 16:45:10 +0200

    Add option to use pass in resignEngine

--- test/engines/resignEngine.js
+++ test/engines/resignEngine.js
@@ -2,18 +2,19 @@ const {Engine} = require('@sabaki/gtp')
 
 let engine = new Engine('Resign Engine', '1.0')
 let alpha = 'ABCDEFGHJKLMNOPQRST'
-let resign = 3
+let stop = 3
+let stopword = process.argv[2] === '--pass' ? 'pass' : 'resign'
 
 for (let commandName of ['boardsize', 'clear_board', 'komi', 'play', 'undo']) {
   engine.command(commandName, '')
 }
 
 engine.command('genmove', (_, out) => {
-  if (resign === 0) {
-    out.send('resign')
+  if (stop === 0) {
+    out.send(stopword)
   } else {
     let rand = () => Math.floor(Math.random() * alpha.length)
-    resign--
+    stop--
 
     out.send(`${alpha[rand()]}${rand() + 1}`)
   }
