efa8d442944b107e8c3ad104517473c452c9435f
Yichuan Shen <shen.yichuan@gmail.com>
2020-02-05 23:44:03 +0100

    Reformat webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,36 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = (env, argv) => ({
-    entry: './src/components/App.js',
+  entry: './src/components/App.js',
 
-    output: {
-        filename: 'bundle.js',
-        path: __dirname
-    },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname
+  },
 
-    devtool: argv.mode === 'production' ? false : 'cheap-module-eval-source-map',
-    target: 'electron-renderer',
+  devtool: argv.mode === 'production' ? false : 'cheap-module-eval-source-map',
+  target: 'electron-renderer',
 
-    node: {
-        __dirname: false
-    },
+  node: {
+    __dirname: false
+  },
 
-    resolve: {
-        alias: {
-            'react': argv.mode === 'production'
-                ? path.join(__dirname, 'node_modules/preact/dist/preact.min.js')
-                : 'preact',
-            'preact': argv.mode === 'production'
-                ? path.join(__dirname, 'node_modules/preact/dist/preact.min.js')
-                : 'preact',
-            'prop-types': path.join(__dirname, 'src/modules/shims/prop-types.js')
-        }
-    },
-
-    externals: {
-        'moment': 'null',
-        'iconv-lite': 'require("iconv-lite")'
+  resolve: {
+    alias: {
+      react:
+        argv.mode === 'production'
+          ? path.join(__dirname, 'node_modules/preact/dist/preact.min.js')
+          : 'preact',
+      preact:
+        argv.mode === 'production'
+          ? path.join(__dirname, 'node_modules/preact/dist/preact.min.js')
+          : 'preact',
+      'prop-types': path.join(__dirname, 'src/modules/shims/prop-types.js')
     }
-})
+  },
+
+  externals: {
+    moment: 'null',
+    'iconv-lite': "require('iconv-lite')"
+  }
+});
