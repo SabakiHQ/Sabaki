8ebb8c1d510de2eb599bda1937bff56ff8ffa9e5
Yichuan Shen <shen.yichuan@gmail.com>
2020-02-15 20:07:15 +0100

    Fix sound.playCapture audio caching

--- src/modules/sound.js
+++ src/modules/sound.js
@@ -6,7 +6,7 @@ const setting = remote.require('./setting')
 function prepareFunction(sounds) {
   let lastIndex = -1
 
-  return async function(delay = 0) {
+  return async (delay = 0) => {
     try {
       let index = 0
 
@@ -30,19 +30,22 @@ export const playPachi = prepareFunction(
   [...Array(5)].map((_, i) => new Audio(`./data/${i}.mp3`))
 )
 
-export const playCapture = async delay => {
-  if (delay == null) {
-    delay = setting.get('sound.capture_delay_min')
-    delay += Math.floor(
-      Math.random() * (setting.get('sound.capture_delay_max') - delay)
-    )
-  }
-
+export const playCapture = (() => {
   let f = prepareFunction(
     [...Array(5)].map((_, i) => new Audio(`./data/capture${i}.mp3`))
   )
-  await f(delay)
-}
+
+  return async delay => {
+    if (delay == null) {
+      delay = setting.get('sound.capture_delay_min')
+      delay += Math.floor(
+        Math.random() * (setting.get('sound.capture_delay_max') - delay)
+      )
+    }
+
+    await f(delay)
+  }
+})()
 
 export const playPass = prepareFunction([new Audio('./data/pass.mp3')])
 
