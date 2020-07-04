39092127e103c50f350175e9a289eb7ae7cbbfb2
Adrian Petrescu <apetresc@gmail.com>
2020-07-03 21:46:38 -0400

    Add support for PandaNet UGF format files
    This is a format commonly used in Japanese sites and servers.

--- src/modules/fileformats/index.js
+++ src/modules/fileformats/index.js
@@ -3,14 +3,15 @@ import i18n from '../../i18n.js'
 import * as sgf from './sgf.js'
 import * as ngf from './ngf.js'
 import * as gib from './gib.js'
+import * as ugf from './ugf.js'
 
 const t = i18n.context('fileformats')
 
-let modules = {sgf, ngf, gib}
+let modules = {sgf, ngf, gib, ugf}
 let extensions = Object.keys(modules).map(key => modules[key].meta)
 let combinedExtensions = [].concat(...extensions.map(x => x.extensions))
 
-export {sgf, ngf, gib}
+export {sgf, ngf, gib, ugf}
 
 export const meta = [
   {name: t('Game Records'), extensions: combinedExtensions},
