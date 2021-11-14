c9d52b26ff120b070a30603b7651e24c0b7d1d0d
Bruno Vasselle <bruno.vasselle@laposte.net>
2021-11-14 16:24:55 +0100

    Use 'Custom Dictionary' to store go vocabulary (persistence in settings)

--- src/main.js
+++ src/main.js
@@ -49,6 +49,14 @@ function newWindow(path) {
     )
   }
 
+  if (setting.get('comments.words')) {
+    setting.get('comments.words').map(
+      (word) => {
+        window.webContents.session.addWordToSpellCheckerDictionary(word)
+      }
+    )
+  }
+
   if (setting.get('window.maximized') === true) {
     window.maximize()
   }
