e7ce943eae1b801f0b71734a5794b4a8efae5723
Bruno Vasselle <bruno.vasselle@laposte.net>
2022-01-05 17:39:42 +0100

    Rename event 'commands' to 'engine-ready'; change state on this event

--- src/modules/sabaki.js
+++ src/modules/sabaki.js
@@ -1774,7 +1774,7 @@ class Sabaki extends EventEmitter {
         })
       })
 
-      syncer.on('commands', () =>
+      syncer.on('engine-ready', id => {
         engine.boot
           .filter(x => (x.match(/^(analyze|black|white)$/) ? x : null))
           .map(role => {
@@ -1784,7 +1784,8 @@ class Sabaki extends EventEmitter {
               this.toggleEnginePlayer(syncer.id, role)
             }
           })
-      )
+        this.setState({lastEngineReadyId: id})
+      })
 
       syncer.controller.start()
 
