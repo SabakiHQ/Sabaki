3296e7bbca6e1d9a705323928daab2f398cb27d4
Bruno Vasselle <bruno.vasselle@laposte.net>
2022-01-06 12:51:10 +0100

    Populating custom dictionary from settings is not needed anymore

--- src/main.js
+++ src/main.js
@@ -49,14 +49,6 @@ function newWindow(path) {
     )
   }
 
-  if (setting.get('comments.words')) {
-    setting.get('comments.words').map(
-      (word) => {
-        window.webContents.session.addWordToSpellCheckerDictionary(word)
-      }
-    )
-  }
-
   if (setting.get('window.maximized') === true) {
     window.maximize()
   }
