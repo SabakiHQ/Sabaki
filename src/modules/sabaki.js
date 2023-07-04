9ed570bbfe0aeca9925f372b0a0abdc9135aeadb
Robert Christ <robert.christ@gmail.com>
2023-07-04 13:16:05 -0400

    Add Internationalization Support for Previous Commit

--- src/modules/sabaki.js
+++ src/modules/sabaki.js
@@ -2768,7 +2768,7 @@ class Sabaki extends EventEmitter {
       },
       {type: 'separator'},
       {
-        label: t('Show Co&mments'),
+        label: t('Toggle Show Comments'),
         click: () => {
           setting.toggle('view.show_comments')
           this.setState(({showCommentBox}) => ({
@@ -2778,7 +2778,7 @@ class Sabaki extends EventEmitter {
       },
       {type: 'separator'},
       {
-        label: 'Annotate',
+        label: t('Annotate'),
         submenu: commentMenu,
       },
     ]
